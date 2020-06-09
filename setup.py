2595bfb03bf0da8ef9e45fa6d1da311e91bc7aed
Mike Sosa <mike.sosa@sofarocean.com>
2020-06-09 16:36:08 -0700

    Clean up and really we are on version 0.1.8

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as file:
 
 setuptools.setup(
     name='pysofar',
-    version='0.1.7',
+    version='0.1.8',
     license='Apache 2 Licesnse',
     install_requires=[
         'requests',
