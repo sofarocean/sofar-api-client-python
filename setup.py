4a3261da59cd8fcad0de2d6fe4874795f8193bbc
pietersmit <pieter@sofarocean.com>
2022-09-01 09:22:00 -0700

    Version number update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as file:
 
 setuptools.setup(
     name='pysofar',
-    version='0.1.13',
+    version='0.1.14',
     license='Apache 2 License',
     install_requires=[
         'requests',
