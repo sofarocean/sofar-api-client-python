d0119ad57dfda8614a9f02ad51851a3220c267c6
Tim Johnson <2293335+tcj@users.noreply.github.com>
2023-06-01 10:46:00 -0700

    Uncomment SST test

--- tests/test_wave_query_class.py
+++ tests/test_wave_query_class.py
@@ -3,10 +3,10 @@ This file is part of pysofar: A client for interfacing with Sofar Oceans Spotter
 
 Contents: Tests for device endpoints
 
-Copyright (C) 2019
+Copyright (C) 2019-2023
 Sofar Ocean Technologies
 
-Authors: Mike Sosa
+Authors: Mike Sosa et al
 """
 from pysofar.sofar import WaveDataQuery
 
@@ -70,18 +70,17 @@ def test_query_no_end():
     assert 'waves' in response
     assert 'wind' in response
 
-# TODO: need to get a viable spotter for testing
-# def test_query_surface_temp():
-#     q.surface_temp(True)
-#     q.limit = 10
-#
-#     q.clear_end_date()
-#     response = q.execute()
-#     q.set_start_date(st)
-#
-#     assert response is not None
-#     assert isinstance(response, dict)
-#
-#     assert 'waves' in response
-#     assert 'wind' in response
-#     assert 'surfaceTemp' in response
+def test_query_surface_temp():
+    q.surface_temp(True)
+    q.limit = 10
+
+    q.clear_end_date()
+    response = q.execute()
+    q.set_start_date(st)
+
+    assert response is not None
+    assert isinstance(response, dict)
+
+    assert 'waves' in response
+    assert 'wind' in response
+    assert 'surfaceTemp' in response
