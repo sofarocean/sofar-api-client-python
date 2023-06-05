934516d61869fd48b203a612980221e0714f79a8
Tim Johnson <2293335+tcj@users.noreply.github.com>
2023-06-05 14:35:51 -0700

    Bump version to 0.1.15

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as file:
 
 setuptools.setup(
     name='pysofar',
-    version='0.1.14',
+    version='0.1.15',
     license='Apache 2 License',
     install_requires=[
         'requests',
