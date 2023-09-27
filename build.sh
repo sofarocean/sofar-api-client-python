3369653a6557047a3ff5d4127441d5a93065064f
Tim Johnson <2293335+tcj@users.noreply.github.com>
2023-09-26 17:18:21 -0700

    Improve build.sh by adding test.pypi.org (#41)
    * Improve build.sh by adding test.pypi.org
    
    * Less specific instructions
    
    * remove old, old deploy.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,54 @@
 #!/bin/bash -e
-pip install twine
-python setup.py sdist bdist_wheel
-# For Test PyPi:
-#twine upload --repository-url https://test.pypi.org/legacy/ dist/*
-# For production PyPi:
-twine upload dist/*
-pip install --index-url https://test.pypi.org/legacy/ --no-deps pysofar
+
+# script to build & push pysofar to testing/production
+
+pip="python3 -m pip"
+
+usage() {
+    echo "$(basename $0) [--test|--prod]"
+    echo
+    echo "supply --test to push to test.pypi.org"
+    echo "supply --prod to push to production PyPI"
+}
+
+setup() {
+    ${pip} install twine
+    python3 setup.py sdist bdist_wheel
+}
+
+testing() {
+    # For Test PyPi:
+    # https://packaging.python.org/en/latest/guides/using-testpypi/
+    #
+    setup
+    # password is an API token.  search for test.pypi.org in password repository
+    twine upload --username __token__ --repository testpypi dist/*
+    ${pip} install --index-url https://test.pypi.org/simple/ --no-deps --upgrade pysofar
+}
+
+production() {
+    # For production PyPi:
+    setup
+    twine upload dist/*
+    ${pip} install --index-url https://test.pypi.org/legacy/ --no-deps pysofar
+}
+
+main() {
+    # execute testing or production according to user wishes
+    if [[ $1 = "--test" ]]
+    then 
+        testing
+    elif [[ $1 = "--prod" ]]
+    then 
+        production
+    else
+        usage
+    fi
+}
+
+# handle args
+case $# in
+    1) main $1;;
+    *) usage; exit 1;;
+esac
 
